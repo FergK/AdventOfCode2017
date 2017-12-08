5a1f085340a55628607cacfeeeb729307934a436
Fergus Kelley <i@fergk.com>
2017-12-08 09:49:18 -0500

    Added days 6 through 8

---
+++ 06-2.js
@@ -0,0 +1,61 @@
+var input = `4	1	15	12	0	9	9	5	5	8	7	3	14	5	12	3`;
+// input = `0	2	7	0`;
+
+input = input.split("\t").map( function(r){ return parseInt(r, 10) } );
+
+function getMaxIndex(a) {
+	let maxIndex = 0;
+	for (var i = 1; i < a.length; i++) {
+		if (a[i] > a[maxIndex]) {
+			maxIndex = i;
+		}
+	}
+	return maxIndex;
+}
+
+function redistribute(a, i) {
+	let blocks = a[i];
+	a[i] = 0;
+	i++;
+	while (blocks > 0) {
+		a[i%a.length]++;
+		i++;
+		blocks--;
+	}
+	return a;
+}
+
+function checkIfLooped(a) {
+	for (var i = 0; i < a.length-1; i++) {
+		if ( arraysEqual(a[i], a[a.length-1]) ) {
+			console.log("looped from: " + i)
+			console.log("a.length-1: " + (a.length-1))
+			console.log("a.length-1-i " + (a.length-1-i))
+			return true;
+		}
+	}
+	return false;
+}
+
+function arraysEqual(a, b) {
+	for (var i = 0; i < a.length; ++i) {
+		if (a[i] !== b[i]) return false;
+	}
+	return true;
+}
+
+let configurations = [];
+let cycles = 0;
+let looped = false;
+while (!looped) {
+	// console.log("input:" + input);
+	input = redistribute(input, getMaxIndex(input));
+	// console.log("redistributed:" + input);
+	configurations.push(input.slice(0));
+	// console.log("configurations:");
+	// console.log(configurations);
+	looped = checkIfLooped(configurations);
+	cycles++;
+}
+// console.log(configurations);
+console.log("cycles: " + cycles);
